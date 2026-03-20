6fa191006110bc5592206d45512f4ff3be810cbf
Aarush Kumar <2005aarushkumar@gmail.com>
2026-03-20 21:15:21 +0530

    fix: profile page render error due to improper handling of products in useUserProducts hook

--- frontend/src/hooks/useUserProducts.js
+++ frontend/src/hooks/useUserProducts.js
@@ -2,12 +2,13 @@ import useProducts from "./useProducts";
 import { useMemo } from "react";
 
 const useUserProducts = (userId) => {
-    const { data: products, isLoading } = useProducts();
+    const { data, isLoading } = useProducts();
 
     const userProducts = useMemo(() => {
-        if (!products || !userId) return [];
+        if (!data || !userId) return [];
+        const products = data.products;
         return products.filter((product) => product.seller?._id === userId);
-    }, [products, userId]);
+    }, [data, userId]);
 
     return {
         data: userProducts,
